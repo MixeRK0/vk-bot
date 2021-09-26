2420b3e891719b5111dd29672cb8d0149b2f95b5
Arhangelski Nikolay <arhangelskinikolay@gmail.com>
2021-09-26 23:45:48 +0300

    temp

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const path = require('path')
-const PORT = process.env.PORT || 5000
 const VkBot = require('node-vk-bot-api');
+const app = express();
 
-const bot = new VkBot('983f0a47fb46b4f66f02b0c2d8496de90f591921a2e9b1e77c05d36e8f5d1dbf82361d21ac835ee9219b4');
+const bot = new VkBot({
+    token: '983f0a47fb46b4f66f02b0c2d8496de90f591921a2e9b1e77c05d36e8f5d1dbf82361d21ac835ee9219b4',
+    confirmation: process.env.CONFIRMATION,
+});
 
 console.log('qwe');
 // bot.command('/start', (ctx) => {
 //     ctx.reply('Hello!');
 // });
-bot.command('Начать', (ctx) => {
-    ctx.reply('Hello!');
-});
-bot.command('Тест', (ctx) => {
-    ctx.reply('Бот работает');
-});
-
-bot.startPolling();
+// bot.command('Начать', (ctx) => {
+//     ctx.reply('Hello!');
+// });
+// bot.command('Тест', (ctx) => {
+//     ctx.reply('Бот работает');
+// });
+//
+// bot.startPolling();
 
 // express()
 //   .use(express.static(path.join(__dirname, 'public')))
@@ -24,3 +28,12 @@ bot.startPolling();
 //   .set('view engine', 'ejs')
 //   .get('/', (req, res) => res.render('pages/index'))
 //   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+bot.on((ctx) => {
+    ctx.reply('Hello!');
+});
+
+app.use(bodyParser.json());
+
+app.post('/', bot.webhookCallback);
+
+app.listen(process.env.PORT);
