a13bb5d14b1f815b3286cab65527942d8f098f2a
Arhangelski Nikolay <arhangelskinikolay@gmail.com>
2021-09-26 20:07:17 +0300

    temp

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const VkBot = require('node-vk-bot-api');
+
+const bot = new VkBot('983f0a47fb46b4f66f02b0c2d8496de90f591921a2e9b1e77c05d36e8f5d1dbf82361d21ac835ee9219b4');
+
+// bot.command('/start', (ctx) => {
+//     ctx.reply('Hello!');
+// });
+bot.command('Начать', (ctx) => {
+    ctx.reply('Hello!');
+});
+bot.command('Тест', (ctx) => {
+    ctx.reply('Бот работает');
+});
+
+bot.startPolling();
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
